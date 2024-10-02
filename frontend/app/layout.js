fb0b4a6b3ada1f071694f2f68a8e34af8f131de9
KabeerH <kabeerharjani@gmail.com>
2024-10-02 12:50:19 -0400

    modified:   backend/server.js - Added Cors modified:   frontend/app/globals.css modified:   frontend/app/layout.js modified:   frontend/app/page.js new file:   frontend/components/Footer.js modified:   frontend/components/NavigationBar.js - ReWorked modified:   frontend/package-lock.json modified:   frontend/package.json

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,6 +1,8 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import NavBar from "@/components/NavigationBar";
+import Footer from "@/components/Footer";
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -23,6 +25,7 @@ export default function RootLayout({ children }) {
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <NavBar />
         {children}
+        <Footer />
       </body>
     </html>
   );
